cf031a24e0cec59c6e2b4b5ff90d49c85b5c58d9
Moiz Ahmed <moizahmedbss786@gmail.com>
2025-03-04 02:24:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,26 @@
 import json
 from datetime import datetime
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Use /mnt/data.json instead of data.json directly
+DATA_FILE_PATH = '/mnt/data.json'
 
 def read_messages_from_file():
-    """ Read all messages from a JSON file"""
-    with open('data.json') as messages_file:
+    with open(DATA_FILE_PATH) as messages_file:
         return json.load(messages_file)
 
-
 def append_message_to_file(content):
-    """ Read the contents of JSON file, add this message to it's contents, then write it back to disk. """
     data = read_messages_from_file()
     new_message = {
         'content': content,
         'timestamp': datetime.now().isoformat(" ", "seconds")
     }
     data['messages'].append(new_message)
-    with open('data.json', mode='w') as messages_file:
+    with open(DATA_FILE_PATH, mode='w') as messages_file:
         json.dump(data, messages_file)
 
-
-# The Flask route, defining the main behaviour of the webserver:
 @app.route("/")
 def home():
     new_message = request.args.get('msg')
@@ -32,6 +28,8 @@ def home():
         append_message_to_file(new_message)
 
     data = read_messages_from_file()
-
-    # Return a Jinja HTML template, passing the messages as an argument to the template:
     return render_template('home.html', messages=data['messages'])
+
+# This part is critical for Docker
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
